a74e89f44d0822bb1f7a5983ab948b94f48e3f42
lilylaptop <lilylaptop@DESKTOP-IUP99PT>
2020-05-07 15:56:40 -0700

    Houses

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,7 +1,70 @@
 package _01_houses;
 
+import java.awt.Color;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class Houses {
-	public void run() {
+	Robot bob = new Robot();
+	public void run(){
+		
+		
+		bob.miniaturize();
+		bob.setSpeed(300);
+		bob.moveTo(50, 500);
+		bob.penDown();
+		
+		for(int i = 0; i <= 10; i++) {
+		drawFlatRoof("", Color.BLUE);
+		
+		}
+	}
+	
+	void drawFlatRoof(String height, Color clr) {
+		bob.setPenColor(clr);
+		int def;
+		
+		if (height .equals("small")) {
+		def = 60;
+		}
+		
+		else if (height .equals ("med")) {
+			def = 120;
+			
+		}
+		
+		else {
+			def = 250;
+			drawPointyRoof(def, Color.BLUE);
+			return;
+		}
+		
+	bob.move(def);
+	bob.turn(90);
+	bob.move(15);
+	bob.turn(90);
+	bob.move(def);
+	bob.turn(-90);
+	bob.setPenColor(0, 225, 0);
+	bob.move(20);
+	bob.turn(-90);
+	}
+	
+	void drawPointyRoof(int def, Color clr) {
+		
+		bob.setPenColor(clr);
+		
+		bob.move(def);
+		bob.turn(40);
+		bob.move(15);
+		bob.turn(100);
+		bob.move(15);
+		bob.turn(40);
+		bob.move(def);
+		bob.turn(-90);
+		bob.setPenColor(0, 225, 0);
+		bob.move(20);
+		bob.turn(-90);
 		
 	}
 }
