3362ca75d4434a4df60a1eff7590ddb27a36ab76
lilylaptop <lilylaptop@DESKTOP-IUP99PT>
2020-06-18 16:01:47 -0700

    fortune cookie

---
+++ src/_07_fortune_cookie/FortuneCookieRunner.java
@@ -0,0 +1,9 @@
+package _07_fortune_cookie;
+
+public class FortuneCookieRunner {
+public static void main(String[] args) {
+	new FortuneCookie().showButton();
+	//FortuneCookie jig = new FortuneCookie();
+	//jig.showButton();
+}
+}
