7e1a0a926e3657991bde2395800c07f704465e63
lilylaptop <lilylaptop@DESKTOP-IUP99PT>
2020-06-26 16:59:50 -0700

    jackInTheBox

---
+++ src/_08_jack_in_the_box/JackInTheBox_Runner.java
@@ -0,0 +1,11 @@
+package _08_jack_in_the_box;
+
+public class JackInTheBox_Runner {
+	public static void main(String[] args) {
+		
+		Jack_In_The_Box idk = new Jack_In_The_Box();
+		idk.Button();
+		
+
+	}
+}
